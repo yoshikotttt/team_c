c2013fc056a2df739f544bb982a6d390576178c7
yoshikotttt <myqic.n@gmail.com>
2023-06-28 22:37:59 +0900

    modelリレーション　追記

--- app/Models/Personality.php
+++ app/Models/Personality.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Personality extends Model
 {
     use HasFactory;
+
+    public function users(): BelongsTo
+{
+    return $this->belongsTo(User::class);
+}
+
+public function sentences(): HasMany
+{
+    return $this->hasMany(Sentence::class);
+}
+
+
 }
--- app/Models/Sentence.php
+++ app/Models/Sentence.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sentence extends Model
 {
     use HasFactory;
+
+    public function personality(): BelongsTo
+    {
+        return $this->belongsTo(Personality::class);
+    }
 }
