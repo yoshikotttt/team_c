f437aa48b0eddec394b42f2b3e3c6b54e6fb210b
yoshikotttt <myqic.n@gmail.com>
2023-06-29 01:28:37 +0900

    文章160個追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this ->call(PersonalitiesTableSeeder::class);
-
+        $this ->call(SentencesTableSeeder::class);
 
     }
 }
