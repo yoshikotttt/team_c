3517ca5aa02f0d17a44d0ac43bb5d1b267ed41ec
E-Shizuka <snowman.r@gmail.com>
2023-06-30 17:00:19 +0900

    詳細画面のリンクを変更しました。（条件分岐で表示する方法に変更します！）

--- resources/js/timeline.js
+++ resources/js/timeline.js
@@ -6,7 +6,7 @@ getAll("users").then((users) => {
         output += `<div>`;
         output += `<h2>${user.name}</h2>`;
         output += `<p>${user.personality_id}</p>`;
-        output += `<button onclick="location.href='/detail?id=${user.id}'" type="button" class="btn btn-primary">詳細を見る</button>`;
+        output += `<button onclick="location.href='detail'" type="button" class="btn btn-primary">詳細を見る</button>`;
         output += `</div>`;
     });
     document.querySelector("#output").innerHTML = output;
