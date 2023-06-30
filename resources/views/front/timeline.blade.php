aa0a0a95dd43b9069ad9899490a9ebfb0ba007f7
E-Shizuka <snowman.r@gmail.com>
2023-06-30 10:59:00 +0900

    フッターのリンクを更新しました。

--- resources/views/front/timeline.blade.php
+++ resources/views/front/timeline.blade.php
@@ -22,13 +22,10 @@
         <!-- フッター（ログイン後固定） -->
         <ul class="grid grid-flow-col text-center text-gray-500 bg-gray-100 rounded-lg p-1">
             <li>
-                <a href="#page1" class="flex justify-center py-4">Home</a>
+                <a href="timeline" class="flex justify-center py-4">Home</a>
             </li>
             <li>
-                <a href="#page2" class="flex justify-center bg-white rounded-lg shadow text-indigo-900 py-4">Timeline</a>
-            </li>
-            <li>
-                <a href="#page3" class="flex justify-center py-4">mypage</a>
+                <a href="mypage" class="flex justify-center py-4">mypage</a>
             </li>
             <li>
                 <a href="#page4" class="flex justify-center py-4">設定</a>
