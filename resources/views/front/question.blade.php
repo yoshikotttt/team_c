2b93324ef286b044ad260fd37da4903977d779e0
E-Shizuka <snowman.r@gmail.com>
2023-06-30 11:59:31 +0900

    質問ページのhtmlを作成

---
+++ resources/views/front/question.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <!-- クリックしたユーザーの情報を取ってくる -->
+    <p>問題！</p>
+    <form id="userForm">
+        @csrf
+
+        <!-- 書き替えるユーザーのid -->
+        <div>
+            <x-text-input id="user_id" class="block mt-1 w-full" type="hidden" name="user_id" value="関数" />
+        </div>
+
+        <!-- 質問 -->
+        <div class="mt-4">
+            <x-input-label for="content" :value="'関数（ユーザー名）さんの誕生日は？'" />
+            <x-text-input id="content" class="block mt-1 w-full" type="date" name="birthday" :value="old('birhday')" required autocomplete="birhday" />
+            <x-input-error :messages="$errors->get('birhday')" class="mt-2" />
+        </div>
+        
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button class="ml-4">
+                {{ '回答' }}
+            </x-primary-button>
+        </div>
+    </form>
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    @vite('resources/js/app.js')
+
+    <!-- Custom JS -->
+    @vite('resources/js/question.js')
+
+</body>
+</html>
