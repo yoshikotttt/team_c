05bfe75a5aaf514398974e92943d5b4b9d4371bf
yoshikotttt <myqic.n@gmail.com>
2023-07-01 17:54:39 +0900

    編集画面　前準備

--- resources/js/edit.js
+++ resources/js/edit.js
@@ -6,10 +6,21 @@ document
     .addEventListener("submit", function (event) {
         event.preventDefault();
 
-        let user_id = document.getElementById("user_id").value;
-        let sentence_id = document.getElementById("sentence_id").value;
-        let edit_user_id = document.getElementById("edit_user_id").value;
+        // let user_id = document.getElementById("user_id").value;
+        // let sentence_id = document.getElementById("sentence_id").value;
+        // let edit_user_id = document.getElementById("edit_user_id").value;
+
+        let user_id = parseInt(document.getElementById("user_id").value, 10);
+        let sentence_id = parseInt(document.getElementById("sentence_id").value, 10);
+        let edit_user_id = parseInt(document.getElementById("edit_user_id").value, 10);
         let content = document.getElementById("content").value;
+
+
+        if (!user_id || !sentence_id || !edit_user_id || !content) {
+            alert("Please fill in all required fields.");
+            return;
+        }
+
 
         let data = {
             user_id: user_id,
@@ -18,8 +29,8 @@ document
             content: content,
         };
 
-        create("edit", data).then((data) => {
-            console.log("Success:", data);
+        create("edit", data).then((newSentence) => {
+            console.log("Success:", newSentence);
             alert("edit created successfully");
         });
     });
