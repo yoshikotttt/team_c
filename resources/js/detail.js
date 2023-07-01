e845bca99e3d7e35b24112dada65a4889855c873
E-Shizuka <snowman.r@gmail.com>
2023-07-01 23:48:57 +0900

    ユーザー名が表示されるように書き替えました！

--- resources/js/detail.js
+++ resources/js/detail.js
@@ -1,21 +1,20 @@
 import { getAll, getOne } from "./api.js";
 
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const id = params.get("id");
 
 // getOne('users', id).then(user => {
 //     return user;
 // });
 
-
-getAll("users").then((users) => {
-    // const username = `${users.name}`;
+getOne("users", id).then((user) => {
+    console.log(user);
     let output1 = "";
-    output1 += `<h2>${users.name}さんはこんな人</h2>`;
+    output1 += `<h2>${user.name}さんはこんな人</h2>`;
     document.querySelector("#name1").innerHTML = output1;
 
     let output2 = "";
-    output2 += `<h2>つまり、${users.name}さんはこんな人</h2>`;
+    output2 += `<h2>つまり、${user.name}さんはこんな人</h2>`;
     document.querySelector("#name2").innerHTML = output2;
 });
 
@@ -29,33 +28,29 @@ getAll("users").then((users) => {
 // });
 
 getAll("sentences").then((sentences) => {
-    getOne('users', id).then(user => {
-      console.log(user.personality_id);
-  
-      let output = ""; // output変数を外側に定義
-  
-      sentences.forEach((item) => {
-        console.log(item.personalities_id);
-  
-        if (user.personality_id == item.personalities_id) { // 比較演算子を修正
-  
-          // 条件に一致した場合の処理を追加
-          output += `<div>`;
-          output += `<div id="${item.id}">`;
-          output += `<p>${item.content}</p>`;
-          output += `</div >`;
-          output += `</div>`;
-        }
-      });
-  
-      // output変数の内容をHTMLに挿入
-      document.querySelector("#output").innerHTML = output;
-  
+    getOne("users", id).then((user) => {
+        console.log(user.personality_id);
+
+        let output = ""; // output変数を外側に定義
+
+        sentences.forEach((item) => {
+            console.log(item.personalities_id);
+
+            if (user.personality_id == item.personalities_id) {
+                // 比較演算子を修正
+
+                // 条件に一致した場合の処理を追加
+                output += `<div>`;
+                output += `<div id="${item.id}">`;
+                output += `<p>${item.content}</p>`;
+                output += `</div >`;
+                output += `</div>`;
+            }
+        });
+        // output変数の内容をHTMLに挿入
+        document.querySelector("#output").innerHTML = output;
     });
-  });
-  
-
-
+});
 
 // getAll("editSentences").then((editSentences) => {
 //     let output = "";
