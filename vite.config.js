3e09fa5cfe23af2fbdb98f7cfda5f66b44f6bde1
E-Shizuka <snowman.r@gmail.com>
2023-07-02 16:48:23 +0900

    detail_edit.jsの記載を追加

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 "resources/js/app.js",
                 "resources/js/api.js",
                 "resources/js/detail.js",
+                "resources/js/detail_edit.js",
                 "resources/js/edit.js",
                 "resources/js/mypage.js",
                 "resources/js/timeline.js",
