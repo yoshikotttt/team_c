8b133f72be6b8a9b6a0e676eb66b6ba3243c40ee
yoshikotttt <myqic.n@gmail.com>
2023-07-03 21:50:53 +0900

    個人設定画面変更

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,10 +1,12 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
@@ -26,4 +28,13 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</body>
+</html>
+    {{-- <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot> --}}
+
+   
+
