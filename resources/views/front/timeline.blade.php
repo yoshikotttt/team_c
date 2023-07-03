587c8cdf556932c578142711ab58654bd0adeec3
E-Shizuka <snowman.r@gmail.com>
2023-07-03 21:51:40 +0900

    検索画面へのリンクを追加、フッターの設定へのリンクを変更

--- resources/views/front/timeline.blade.php
+++ resources/views/front/timeline.blade.php
@@ -30,7 +30,7 @@
                 <a href="mypage" class="flex justify-center py-4">mypage</a>
             </li>
             <li>
-                <a href="setting" class="flex justify-center py-4">設定</a>
+                <a href="/profile" class="flex justify-center py-4">設定</a>
             </li>
         </ul>
      <!-- JS, Popper.js, and jQuery -->
