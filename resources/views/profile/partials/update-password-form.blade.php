f6bf55ea887ee705c793aa6dadbf45ee1e97fffe
E-Shizuka <snowman.r@gmail.com>
2023-07-05 01:45:44 +0900

    色の変更漏れを追加

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,7 +1,7 @@
 <section class="flex items-center justify-center">
     <div class="max-w-md w-full">
 
-        <h2 class="text-center text-xl font-semibold text-gray-900 dark:text-white">パスワード変更</h2>
+        <h2 class="text-center text-xl font-semibold text-indigo-900">パスワード変更</h2>
 
 
        
@@ -12,28 +12,28 @@
 
             <div class="m-auto w-full max-w-xs">
                 <div class="mt-6">
-                    <label for="current_password" class="block font-medium text-sm text-gray-700">現在のパスワード</label>
+                    <label for="current_password" class="block font-medium text-sm text-indigo-900">現在のパスワード</label>
                     <input id="current_password" name="current_password" type="password"
-                        class="input input-bordered input-primary w-full max-w-xs mb-2" autocomplete="current-password" />
+                        class="input input-bordered input-primary w-full max-w-xs mb-2 text-indigo-900" autocomplete="current-password" />
                     <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
                 </div>
 
                 <div>
-                    <label for="password" class="block font-medium text-sm text-gray-700">新しいパスワード</label>
+                    <label for="password" class="block font-medium text-sm text-indigo-900">新しいパスワード</label>
                     <input id="password" name="password" type="password" 
-                    class="input input-bordered input-primary w-full max-w-xs mb-2" autocomplete="new-password" />
+                    class="input input-bordered input-primary w-full max-w-xs mb-2 text-indigo-900" autocomplete="new-password" />
                     <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
                 </div>
 
                 <div>
-                    <label for="password_confirmation" class="block font-medium text-sm text-gray-700">新しいパスワード（確認）</label>
+                    <label for="password_confirmation" class="block font-medium text-sm text-indigo-900">新しいパスワード（確認）</label>
                     <input id="password_confirmation" name="password_confirmation" type="password" 
-                    class="input input-bordered input-primary w-full max-w-xs" autocomplete="new-password" /> 
+                    class="input input-bordered input-primary w-full max-w-xs text-indigo-900" autocomplete="new-password" /> 
                     <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
                 </div>
 
                 <div class="flex items-center justify-center gap-4">
-                    <button type="submit" class="bg-purple-300 hover:bg-purple-200 text-white font-bold py-2 px-4 rounded mt-6">
+                    <button type="submit" class="btn bg-rose-200 btn-sm text-indigo-900 mt-4">
                         {{ __('更新') }}
                     </button>
 
