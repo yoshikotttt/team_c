8a91f2f51cd27b1b669feed098b99f5e6ff8cff4
E-Shizuka <snowman.r@gmail.com>
2023-07-05 01:02:04 +0900

    色の記述を追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
         <link href="https://cdn.jsdelivr.net/npm/daisyui@3.1.7/dist/full.css" rel="stylesheet" type="text/css" />
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body class="flex items-center justify-center h-screen">
+    <body class="flex items-center justify-center h-screen bg-yellow-50">
         <div class="m-auto inset-0">
             <div>
-                <div class="card m-2 mx-6 bg-accent text-primary-content">
+                <div class="card m-2 mx-6 bg-emerald-200 text-primary-content">
                     <div class="card-body">
-                        <h2 class="text-center text-xl font-semibold text-gray-900 dark:text-white">性格診断アプリ<br>〇〇</h2>
+                        <h2 class="text-center text-xl font-semibold text-indigo-900 ">性格診断アプリ<br>〇〇</h2>
                     </div>
                 </div>
                 <div class="flex justify-center">
@@ -25,25 +25,25 @@
                 </div>
 
                 <div class="mt-6">
-                    <div class="card m-2 mx-12 mb-6 bg-primary text-primary-content">
+                    <div class="card m-2 mx-12 mb-6 bg-amber-200 text-primary-content">
                         <div class="card-body">
                             <a href="{{ route('explanation') }}">
                             
-                            <h2 class="text-center text-xl font-semibold text-gray-900 dark:text-white">このアプリの使い方</h2>
+                            <h2 class="text-center text-xl font-semibold text-indigo-900">このアプリの使い方</h2>
 
-                            <p class="mt-1 text-center text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            <p class="mt-1 text-center text-indigo-900 text-sm leading-relaxed">
                                 初めて使う方はこちら
                             </p>
                             </a>
                         </div>
                     </div>
-                    <div class="card m-2 mx-12 mb-6 bg-primary text-primary-content">
+                    <div class="card m-2 mx-12 mb-6 bg-amber-200 text-primary-content">
                         <div class="card-body">
                             <a href="https://www.16personalities.com/ja" target="_blank">
                             
-                            <h2 class="text-center text-xl font-semibold text-gray-900 dark:text-white">16タイプ性格診断</h2>
+                            <h2 class="text-center text-xl font-semibold text-indigo-900">16タイプ性格診断</h2>
 
-                            <p class="mt-1 text-center text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            <p class="mt-1 text-center text-indigo-900 text-sm leading-relaxed">
                                 外部サイトへ移動します
                             </p>
                             </a>
@@ -57,13 +57,13 @@
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                         
                     @else
-                        <button onclick="location.href='login'" type="button" class="btn btn-active btn-success btn-sm mx-2">
+                        <button onclick="location.href='login'" type="button" class="btn btn-active bg-rose-200 btn-sm mx-2 text-indigo-900">
                             ログイン
                         </button>
                       
 
                         @if (Route::has('register'))
-                            <button onclick="location.href='register'" type="button" class="btn btn-active btn-info btn-sm mx-2">
+                            <button onclick="location.href='register'" type="button" class="btn btn-active bg-blue-200 btn-sm mx-2 text-indigo-900">
                                 新規登録
                             </button>
                         @endif
