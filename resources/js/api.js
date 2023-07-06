a7a30f35ca25b5c9303c4351bfec19c26bec64e6
E-Shizuka <snowman.r@gmail.com>
2023-07-07 00:23:47 +0900

    urlを再度修正

--- resources/js/api.js
+++ resources/js/api.js
@@ -5,28 +5,28 @@ import { handleError } from "./functions.js";
 const projectName = "project";
 export function getAll(endpoint) {
     return axios
-        .get(`${projectName}/api/${endpoint}`)
+        .get(`/${projectName}/api/${endpoint}`)
         .then((response) => response.data)
         .catch(handleError);
 }
 
 export function getOne(endpoint, id) {
     return axios
-        .get(`${projectName}/api/${endpoint}/${id}`)
+        .get(`/${projectName}/api/${endpoint}/${id}`)
         .then((response) => response.data)
         .catch(handleError);
 }
 
 export function create(endpoint, data) {
     return axios
-        .post(`${projectName}/api/${endpoint}`, data)
+        .post(`/${projectName}/api/${endpoint}`, data)
         .then((response) => response.data)
         .catch(handleError);
 }
 
 export function update(endpoint, id, data) {
     return axios
-        .put(`${projectName}/api/${endpoint}/${id}`, data)
+        .put(`/${projectName}/api/${endpoint}/${id}`, data)
         .then((response) => response.data)
         .catch(handleError);
 }
@@ -34,14 +34,14 @@ export function update(endpoint, id, data) {
 //質問の回答の正誤判断のために追加
 export function search(endpoint, params) {
     return axios
-        .get(`${projectName}/api/${endpoint}`, { params })
+        .get(`/${projectName}/api/${endpoint}`, { params })
         .then((response) => response.data)
         .catch(handleError);
 }
 
 export function remove(endpoint, id) {
     return axios
-        .delete(`${projectName}/api/${endpoint}/${id}`)
+        .delete(`/${projectName}/api/${endpoint}/${id}`)
         .then((response) => response.data)
         .catch(handleError);
 }
